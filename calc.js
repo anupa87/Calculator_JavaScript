21433b9cde42da157b289b558189d583c65d8928
Anupa87 <thapaanupa@gmail.com>
2022-08-31 14:58:13 +0300

    BMI calculator added , needs some small fixes

--- calc.js
+++ calc.js
@@ -1,9 +1,14 @@
 var numField1 = document.getElementById("numField1");
 var numField2 = document.getElementById("numField2");
 var resultFiled = document.getElementById("resultField");
-var form = document.getElementById("xIsWhatPercentOfY");
+var percentForm = document.getElementById("xIsWhatPercentOfY");
 
-form.addEventListener("submit", function (event) {
+var wtField = document.getElementById("wtField");
+var htField = document.getElementById("htField");
+var bmiResult = document.getElementById("bmiResult");
+var bmiForm = document.getElementById("bmiForm");
+
+percentForm.addEventListener("submit", function (event) {
   if (!numField1.value || !numField2.value) {
     alert("Please enter values in the fields");
   } else {
@@ -14,6 +19,21 @@ form.addEventListener("submit", function (event) {
     var percent = result * 100;
 
     resultFiled.innerText = "Result: " + percent + "%";
+    event.preventDefault();
+  }
+});
+
+bmiForm.addEventListener("submit", function (event) {
+  if (!wtField.value || !htField.value) {
+    alert("Please enter values in the fields");
+  } else {
+    var wt = wtField.value;
+    var ht = htField.value;
+
+    var result = wt / (ht * ht);
+    var bmi = result * 10000;
+
+    bmiResult.innerText = "Result: " + bmi + "kg/m2";
     event.preventDefault();
   }
 });
