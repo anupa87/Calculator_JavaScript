58c62b6b8e6abbd23ed61e8c89bd30344e186d26
Anupa87 <thapaanupa@gmail.com>
2022-08-31 13:47:48 +0300

    css-added

--- calc.js
+++ calc.js
@@ -3,12 +3,17 @@ var numField2 = document.getElementById("numField2");
 var resultFiled = document.getElementById("resultField");
 var form = document.getElementById("xIsWhatPercentOfY");
 
-form.addEventListener("submit", function () {
+form.addEventListener("submit", function (event) {
   if (!numField1.value || !numField2.value) {
     alert("Please enter values in the fields");
   } else {
     var x = parseFloat(numField1.value);
     var y = parseFloat(numField2.value);
-    alert(x + y);
+
+    var result = x / y;
+    var percent = result * 100;
+
+    resultFiled.innerText = "Result: " + percent + "%";
+    event.preventDefault();
   }
 });
